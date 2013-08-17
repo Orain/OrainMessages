5b2cbb32a4adb61a0883f3cd62de44aa2bd69400
Hydriz Scholz <Hydriz@users.noreply.github.com>
2013-08-17 10:37:26 +0800

    Adding Chinese (Simplified) and Chinese (Traditional) translations

--- OrainMessages.i18n.php
+++ OrainMessages.i18n.php
@@ -22,3 +22,33 @@ $messages['en'] = array(
 	'sitesupport-url'        => '',
 	'tooltip-n-sitesupport'  => '',
 );
+
+/** Chinese (Simplified)
+ * @author Hydriz
+ */
+$messages['zh-hans'] = array(
+	'centralauth-groupname'  => '奥兰',
+	'orainmessages-desc'     => '奥兰的特别信息',
+	'sitematrix'             => '奥兰维基列表',
+	'sitematrix-summary'     => '此特殊页面列出所有奥兰维基。',
+	'sitematrix-desc'        => '显示一个[[Special:SiteMatrix|奥兰维基列表]]',
+	'sitematrix-others'      => '其他奥兰维基',
+	'sitesupport'            => '',
+	'sitesupport-url'        => '',
+	'tooltip-n-sitesupport'  => '',
+);
+
+/** Chinese (Traditional)
+ * @author Hydriz
+ */
+$messages['zh-hant'] = array(
+	'centralauth-groupname'  => '奧蘭',
+	'orainmessages-desc'     => '奧蘭的特別信息',
+	'sitematrix'             => '奧蘭維基列表',
+	'sitematrix-summary'     => '此特殊頁面列出所有奧蘭維基。',
+	'sitematrix-desc'        => '顯示一個[[Special:SiteMatrix|奧蘭維基列表]]',
+	'sitematrix-others'      => '其他奧蘭維基',
+	'sitesupport'            => '',
+	'sitesupport-url'        => '',
+	'tooltip-n-sitesupport'  => '',
+);
